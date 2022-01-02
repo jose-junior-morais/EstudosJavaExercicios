3d6b86f9312adb6266359058fd8d646f7a77c304
Jose Junior <juniormilian6@gmail.com>
2022-01-02 17:05:11 -0300

    Hashcode e Equals

---
+++ src/applicaton/hashcode/equals/Program.java
@@ -0,0 +1,17 @@
+package applicaton.hashcode.equals;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String a="jj";
+		String b="bb";
+		
+		System.out.println(a.equals(b));
+		
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+				
+	}
+
+}
